17d1105901db7e6fa8555bd08eb2d161c549c7a7
Ludvablack <107507944+Ludvablack@users.noreply.github.com>
2026-04-09 21:43:47 +0200

    Delete jobmarket/jobmarket/connection.php

--- jobmarket/jobmarket/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = 'localhost';
-$uzivatel = 'root';
-$heslo = '';
-$databaze = 'jobmarket';
-$conn = new mysqli($server, $uzivatel, $heslo, $databaze);
-// tady program nacte databazi
